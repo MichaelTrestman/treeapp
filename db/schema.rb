70689cdfbb12f9a5aa18d1e72be4b7f5053f1dfd
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-09 12:10:20 -0800

    migration and schema update for creating publications

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109011510) do
+ActiveRecord::Schema.define(version: 20150109190642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "publications", force: true do |t|
+    t.string   "title"
+    t.string   "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", force: true do |t|
     t.string   "title"

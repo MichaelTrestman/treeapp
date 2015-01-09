85da429230fefac4fbd9c57958e03ffec62b4a8d
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-09 12:11:12 -0800

    adds publication logging script to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require_relative "publication_seeds"
+
+def log_pubs pubs
+  pubs.each do |pub|
+    if Publication.where({title: pub[:title]}).count > 0
+      db_pub = Publication.where({title: pub[:title]})[0]
+    else
+      db_pub = Publication.create({
+        title: pub[:title],
+        date: pub[:publication_date]
+      })
+    end
+
+    # pub[:authors].each do |auth|
+    #   this_author = {}
+    #   this_author[:name] = auth.split(',')
+    #   this_author[:last_name] = this_author[:name][0]
+    #   this_author[:first_name] = this_author[:name][1]
+
+    #   if db_pub.authors.any?{ |db_auth|
+    #     this_author[:first_name] + this_author[:last_name] == db_auth.first_name + db_auth.last_name
+    #   }
+    #     {message: 'author previously in list'}
+    #   elsif Researcher.any?{ |db_auth|
+    #     this_author[:first_name] + this_author[:last_name]  == db_auth.first_name + db_auth.last_name
+    #   }
+    #     new_author = Researcher.where({
+    #       first_name: this_author[:first_name],
+    #       last_name: this_author[:last_name]
+    #     })[0]
+    #     db_pub.authors << new_author
+    #     # new_author.publications << db_pub
+    #   else
+    #     new_author = Researcher.create({
+    #       first_name: this_author[:first_name],
+    #       last_name: this_author[:last_name]
+    #     })
+    #     db_pub.authors << new_author
+    #     # new_author.publications << db_pub
+    #   end
+    # end
+  end
+end
+
+log_pubs $pub_seeds
+

7894b335e2b07731bbbc36f0161a980a0197aec1
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-09 12:12:21 -0800

    adds some publication seeds

---
+++ db/publication_seeds.rb
@@ -0,0 +1,398 @@
+$pub_seeds = [
+  {
+    title: "A Bat Without Qualities",
+    publication_date: "1993",
+    authors: ["Akins, K.A."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Review of Griffin 1992",
+    publication_date: "1992",
+    authors: ["Alcock, J."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Mental Content.",
+    publication_date: "1992",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Mental Content and Evolutionary Explanation.",
+    publication_date: "1992",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Intentionality: Natural and Artificial.",
+    publication_date: "1995",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Animal Cognition and Animal Minds.",
+    publication_date: "1997",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "The Discovery of Animal Consciousness: An Optimistic Assessment.",
+    publication_date: "1997",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "A skeptic's progress.",
+    publication_date: "2002",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Animal Pain.",
+    publication_date: "2004",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Is Anyone a Cognitive Ethologist?",
+    publication_date: "2004",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Fish cognition and consciousness.",
+    publication_date: "2013",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Species of Mind: The Philosophy and Biology of Cognitive Ethology.",
+    publication_date: "1997",
+    authors: ["Allen, C.", "Beckoff, M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Deciphering Animal Pain.",
+    publication_date: "2005",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The Lower Bounds of Cognition: What do spinal cords reveal?",
+    publication_date: "2009",
+    authors: ["Allen, C.", "Grau, J.", "Meagher, M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The Evolution of Reference",
+    publication_date: "1998",
+    authors: ["Allen, C.", "Saidel, E."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Doctor Dolittle's Delusion.",
+    publication_date: "2004",
+    authors: ["Anderson, S.R."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The first step in the case for great ape equality: the argument for other minds.",
+    publication_date: "1996",
+    authors: ["Andrews, K."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Do Apes Read Minds? Toward a New Folk Psychology.",
+    publication_date: "2012",
+    authors: ["Andrews, K."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Evolution of Consciousness: Report of Agora Workshop in Sigtuna, Sweden, August 2001",
+    publication_date: "2002",
+    authors: ["Arhem, P.", "Liljenstrom, H.", "Lindahl, B.I.B."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The Nature of Mind and Other Essays",
+    publication_date: "1980",
+    authors: ["Armstrong, D.A."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Affarent and efferent connections of the dorsolateral corticoid area in comparison with connections of the temporoparieto-occipital area in the pigeon (Columbia livia).",
+    publication_date: "2005",
+    authors: ["Atoji, Y.", "Wild, J.M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "In the Theatre of Consciousness: Global Workspace Theory, A Rigorous Scientific Theory of Consciousness.",
+    publication_date: "1997",
+    authors: ["Baars, B.J."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Experiments and observations on the singing of birds.",
+    publication_date: "1773",
+    authors: ["Barrington, D."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The Emotional Lives of Animals.",
+    publication_date: "2007",
+    authors: ["Beckoff, M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "Cognitive Ethology: Slayers, Skeptics, and Proponents.",
+    publication_date: "1997",
+    authors: ["Beckoff, M.", "Allen, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "The Cognitive Animal.",
+    publication_date: "2002",
+    authors: ["Beckoff, M.", "Allen, C.", "Burghardt, G.M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    title: "The Cambrian explosion and the origins of embodied cognition.",
+    publication_date: "2013",
+    authors: ["Trestman, Michael"],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: ["vertebrates", "cephalodopods", "arthropods"],
+    referenced_taxa: []
+  },
+
+  {
+    title: "The biological basis of speech: What to infer from talking to the animals.",
+    publication_date: "2001",
+    authors: ["Trout, J.D."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "Memory and consciousness.",
+    publication_date: "1985",
+    authors: ["Tulving, E."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "Consciousness, Color, and Content.",
+    publication_date: "2000",
+    authors: ["Tye, M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "In Nature's Interests?",
+    publication_date: "1998",
+    authors: ["Varner, G."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "Personhood, Ethics, and Animal Cognition: Situating Animals in Hare's Two Level Utilitarianism",
+    publication_date: "2012",
+    authors: ["Varner, G."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The evolution of consciousness.",
+    publication_date: "2013",
+    authors: ["Velmans, M."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "The philosophy of bird's nests.",
+    publication_date: "1867",
+    authors: ["Wallace, A.R."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Comparative and Evolutionary Aspects of Nociceptor Function.",
+    publication_date: "1996",
+    authors: ["Walters, E.T."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "The Ways of Behaviorism",
+    publication_date: "1928",
+    authors: ["Watson, J.B."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "The Natural History of Selbourne.",
+    publication_date: "1789",
+    authors: ["White, G."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "Cognitive abilities of araneophagic jumping spiders.",
+    publication_date: "1998",
+    authors: ["Wilcox, R.S.", "Jackson, R.R."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Is consciousness important?",
+    publication_date: "1984",
+    authors: ["Wilkes, K."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Animal ideas.",
+    publication_date: "1995",
+    authors: ["Wilson, M.D."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Do Animals Think?",
+    publication_date: "2004",
+    authors: ["Wynne, C."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Empathy and direct social perception.",
+    publication_date: "2011",
+    authors: ["Zahavi, D."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  }
+]

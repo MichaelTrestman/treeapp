67fed68741a438a92176f7fa71c9e3bb33b0c6d4
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-10 01:09:14 -0800

    wip

--- db/biblio_parser.rb
+++ db/biblio_parser.rb
@@ -1,1 +1,60 @@
-biblio_parser.rb
+File.open('db/biblio.txt').readlines.each do |line|
+
+  previous_name = ""
+
+  lineAry = line.split('(')
+  names = lineAry.shift
+  lineAry = lineAry[0].split(')')
+  date = lineAry.shift
+  remainder = lineAry[0]
+  # p names
+  p date
+  raise 'pooballz' unless remainder.split('')[1] == ' '
+  remainder = remainder.split('').slice(2, 9999).join
+  title = remainder.split('.')[0]
+  p title
+
+  unless names == '––– ' || names == 'Institute for Laboratory Animal Research '
+    names = names.split(',').select{|name| name != ' et al. ' && name != 'InstituteforLaboratoryAnimalResearch'}.map{|name| name.gsub(' ', '').gsub('&', '')}
+    authors = []
+    while names.length > 0
+      raise "fucked up number of name segments; names: #{names}" unless names.length % 2 == 0
+      last_name = names.shift
+      first_name = names.shift
+      authors << {
+        first_name: first_name,
+        last_name: last_name
+      }
+      puts authors
+    end
+    if Publication.where({title: title, date: date}).count
+      new_pub = Publication.where({
+        title: title,
+        date: date
+      })[0]
+    else
+      new_pub = Publication.create({
+        title: title,
+        date: date
+      })
+    end
+    authors.each do |author|
+      unless new_pub.authors.any? {|auth|
+        (auth.first_name == author.first_name) && (auth.last_name == author.last_name)
+      }
+        thisAuth = Author.where({
+          first_name: author.first_name,
+          last_name: author.last_name
+        })
+        if thisAuth.count
+          new_pub.authors << thisAuth[0]
+        else
+          new_pub.authors << Author.create({
+            first_name: author.first_name,
+            last_name: author.last_name
+          })
+        end
+      end
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,87 @@
 
+File.open('db/biblio.txt').readlines.each do |line|
+
+  previous_name ||= ""
+
+  lineAry = line.split('(')
+  names = lineAry.shift
+  lineAry = lineAry[0].split(')')
+  date = lineAry.shift
+  remainder = lineAry[0]
+  # p names
+  p date
+  raise 'pooballz' unless remainder.split('')[1] == ' '
+  remainder = remainder.split('').slice(2, 9999).join
+  title = remainder.split('.')[0]
+  p title
+
+  # unless names == '––– ' || names == 'Institute for Laboratory Animal Research '
+
+  authors = []
+  names = names.split(',').select{|name| name != ' et al. ' && name != 'InstituteforLaboratoryAnimalResearch'}.map{|name| name.gsub(' ', '').gsub('&', '')}
+  p names[0]
+
+  if names[0] == "--- " or names[0] == "---"
+
+    authors << Author.where({
+      first_name: previous_name[:first_name],
+      last_name: previous_name[:last_name]
+    })[0]
+
+  elsif names == 'Institute for Laboratory Animal Research '
+    authors << {
+      first_name: 'The',
+      last_name: names
+    }
+  else
+    while names.length > 0
+      unless names[0] == '___'
+        # raise "fucked up number of name segments; names: #{names}" unless names.length % 2 == 0
+        last_name = names.shift
+        first_name = names.shift
+        previous_name = {
+          first_name: first_name,
+          last_name: last_name
+        }
+        authors << {
+          first_name: first_name,
+          last_name: last_name
+        }
+      end
+      puts authors
+    end
+    if Publication.where({title: title, date: date}).count < 0
+      new_pub = Publication.where({
+        title: title,
+        date: date
+      })[0]
+    else
+      new_pub = Publication.create({
+        title: title,
+        date: date
+      })
+    end
+    authors.each do |author|
+      unless new_pub.authors.any? {|auth|
+        (auth.first_name == author[:first_name]) && (auth.last_name == author[:last_name])
+      }
+        thisAuth = Author.where({
+          first_name: author[:first_name],
+          last_name: author[:last_name]
+        })
+        if thisAuth.count < 0
+          new_pub.authors << thisAuth[0]
+        else
+          new_pub.authors << Author.create({
+            first_name: author[:first_name],
+            last_name: author[:last_name]
+          })
+        end
+      end
+    end
+  end
+end
+
 File.open('db/topic_seeds.txt').readlines.each do |line|
   lineArr = line.split(' ')
   if lineArr[0] == '*'

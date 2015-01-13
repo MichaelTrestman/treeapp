89eb2208e190369439c244ae7a903f32ac65ccea
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-12 22:09:12 -0800

    fixes update redirect issue

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -95,14 +95,12 @@ class PublicationsController < ApplicationController
   # PATCH/PUT /publications/1.json
   def update
 
-
-
     if @publication.update(publication_params)
-      # format.html { redirect_to @publication, notice: 'Publication was successfully updated.' }
-      # format.json { render :show, status: :ok, location: @publication }
+      format.html { redirect_to @publication, notice: 'Publication was successfully updated.' }
+      format.json { render :show, status: :ok, location: @publication }
     else
-      # format.html { render :edit }
-      # format.json { render json: @publication.errors, status: :unprocessable_entity }
+      format.html { render :edit }
+      format.json { render json: @publication.errors, status: :unprocessable_entity }
     end
 
   end

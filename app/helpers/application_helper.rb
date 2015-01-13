e651334832bb314948c071449d243d3352979aeb
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-13 13:10:21 -0800

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def my_name
+    self.name
+  end
+  def searx filters_hash={}, order=nil
+    my_name.constantize.all
+  end
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,7 +3,7 @@
 # require 'debugger'
 
 class Publication < ActiveRecord::Base
-
+  extend ApplicationHelper
   has_many :authorships
   has_many :authors, through: :authorships
 

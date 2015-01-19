71f549e62bf158dc11ddbebc78ba470c739f9c45
Michael Trestman <michael.a.trestman@gmail.com>
2015-01-19 15:31:27 -0800

    cleans up whitespace

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -6,12 +6,10 @@ class Publication < ActiveRecord::Base
   extend PublicationsHelper
   has_many :authorships
   has_many :authors, through: :authorships
-
   # has_many :citations
   # has_many :references, through: :citations
   # has_many :citations
   # has_many :references, through: :citations
-
   # has_many :distributions
   # has_many :taxons, through: :distributions
   has_many :tags
